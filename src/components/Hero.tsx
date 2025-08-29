bb81e4e637d160da7795fcbe1304ddfda554f272
writesunil21 <writesunil21@gmail.com>
2025-08-29 15:31:27 +0530

    download option deployed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,7 +38,7 @@ export default function Hero() {
             Hire Me
           </a>
           <a
-            href="/resume.pdf"
+            href="/resume.pdf" download
             className="px-6 py-3 border border-white rounded-lg hover:bg-white/10"
           >
             Download Resume
